82a6b12f23deff3569d13dbc513b4a3331be8f47
Grigoriy Belenkiy <grishace@gmail.com>
2020-05-22 17:45:01 -0600

    Initial commit.

---
+++ App.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using OpenCvSharp;
+
+namespace opencvw
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public VideoCapture capture { get; private set; }
+        public CascadeClassifier face { get; private set; }
+        public CascadeClassifier smile { get; private set; }
+
+        override protected void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            capture = new VideoCapture(0, VideoCaptureAPIs.DSHOW);
+            face = new CascadeClassifier("haarcascade_frontalface_alt2.xml");
+            smile = new CascadeClassifier("haarcascade_smile.xml");
+        }
+
+        override protected void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            if (capture != null) {
+                capture.Release();
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.IO;
+
+namespace opencvw
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromMilliseconds(40);
+            timer.Tick += (s, e) => Tick();
+            timer.Start();
+        }
+
+        private BitmapImage BitmapToImageSource(System.Drawing.Bitmap bitmap)
+        {
+            using (MemoryStream memory = new MemoryStream())
+            {
+                bitmap.Save(memory, System.Drawing.Imaging.ImageFormat.Bmp);
+                memory.Position = 0;
+                BitmapImage bitmapimage = new BitmapImage();
+                bitmapimage.BeginInit();
+                bitmapimage.StreamSource = memory;
+                bitmapimage.CacheOption = BitmapCacheOption.OnLoad;
+                bitmapimage.DecodePixelWidth = (int) img.Width;
+                bitmapimage.EndInit();
+
+                return bitmapimage;
+            }
+        }
+
+        private void Tick ()
+        {
+            var app = Application.Current as App;
+            using (var frame = new OpenCvSharp.Mat())
+            {
+                var success = app.capture.Read(frame);
+                if (success)
+                {
+                    var f = app.face.DetectMultiScale(frame);
+                    foreach (var r in f) {
+                        OpenCvSharp.Cv2.Rectangle(frame, r, OpenCvSharp.Scalar.Green, 3);
+                    }
+
+                    if (f.Length > 0)
+                    {
+                        var s = app.smile.DetectMultiScale(frame, 1.9, 30, OpenCvSharp.HaarDetectionType.FindBiggestObject);
+                        foreach (var r in s) {
+                            OpenCvSharp.Cv2.Rectangle(frame, r, OpenCvSharp.Scalar.Red, 3);
+                        }
+                    }
+
+                    this.Background = f.Length > 0 ? Brushes.White : Brushes.Green;
+
+                    using (var bmp = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(frame))
+                    {
+                        img.Source = BitmapToImageSource(bmp);
+                    }
+                }
+                else
+                {
+                    this.Background = Brushes.Red;
+                }
+            }
+        }
+    }
+}
